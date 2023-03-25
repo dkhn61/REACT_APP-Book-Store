84dddfbde620bde67bd7c16542a06bc4813b2919
dkhn61 <dkhan6327@gmail.com>
2023-03-24 22:20:29 -0400

    Handling Delete actions on books through State

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,7 +1,34 @@
-function BookShow({book}) {
+import BookEdit from "./BookEdit";
+import { useState } from "react";
+
+function BookShow({ book, onDelete }) {
+    
+    const handleDeleteClick = () => {
+        onDelete(book.id);
+    }
+
+    const [showEdit, setShowEdit] = useState(false);
+
+    const handleEditClick = () => {
+        setShowEdit(!showEdit);
+    };
+
+    let content = <h3>{book.title}</h3>;
+    if (showEdit) {
+        content = <BookEdit/>
+    }
+
     return (
         <div className="book-show">
-            {book.title}
+            <div>{content}</div>
+            <div className="actions">
+                 <button className="edit" onClick={handleEditClick}>
+                    Edit
+                </button>
+                <button className="delete" onClick={handleDeleteClick}>
+                    Delete
+                </button>
+            </div>
         </div>
     );
 }
